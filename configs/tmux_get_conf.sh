e926a878e774f8a4446a7c0762f9a1b8dbe76ae8
Anton Kuscher <antonkuscher@outlook.de>
2026-06-12 20:42:41 +0200

    tmux conf update

--- configs/tmux_get_conf.sh
+++ configs/tmux_get_conf.sh
@@ -12,11 +12,27 @@ set -g pane-base-index 1
 # RGB color support
 set -sg terminal-overrides ",*:RGB"
 
-# Keybindings: split
+###################
+# Keybindinds
 bind h split-window -h
 bind | split-window -h
 bind v split-window -v
 bind - split-window -v
+
+###################
+# Status bar styling
+set -g status-style bg=default,fg=default
+set -g status-justify centre
+set -g status-left "#[bg=#698DDA,fg=#000000] #(whoami)@#S #[bg=default]#[fg=#698DDA]"
+set -g status-right "#[fg=#698DDA,bg=default]#[bg=#698DDA,fg=#000000] %a %d %b %H:%M "
+set -g window-status-current-format "#[fg=#698DDA]#[bg=#698DDA,fg=#000000] #I:#{?#{m:ssh*,#{pane_current_command}},#(ps -t #{pane_tty} -o args= | grep '^ssh ' | sed 's/.* //'),#W} #{?window_zoomed_flag,󰊓 ,}#[fg=#698DDA,bg=default]"
+
+set -g window-status-last-style "fg=default,bg=default"
+
+set -g window-status-format "#[fg=#3a5070]#[bg=#3a5070,fg=#aaaaaa] #I:#W #[bg=default]#[fg=#3a5070]"
+
+set -g status-right-length 30
+set -g status-left-length 30
 EOF
 
 echo "alias ta='tmux attach-session'" >> /home/$(whoami)/.bashrc
